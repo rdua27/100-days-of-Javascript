70361468ddcc97e42f6723db4bf8697a43c7fc84
Rajat Dua <rajatdua1994@gmail.com>
2024-01-02 12:47:54 +0530

    RPG: write methods for basic navigation
    Written methods for basic game navigation like go to Town , Store, cave and completed logic for buy weapon, health

--- Role Playing Game/script.js
+++ Role Playing Game/script.js
@@ -14,7 +14,7 @@ const text = document.querySelector("#text");
 
 const xpText = document.querySelector("#xpText");
 const healthText = document.querySelector("#healthText");
-const goldText = docoument.querySelector("#goldText");
+const goldText = document.querySelector("#goldText");
 
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
@@ -44,3 +44,100 @@ const monsters = [
     health: 300,
   },
 ];
+
+const locations = [
+  {
+    name: "town square",
+    "button text": ["Go to store", "Go to cave", "Fight dragon"],
+    "button functions": [goStore, goCave, fightDragon],
+    text: 'You are in the town square. You see a sign that says "Store".',
+  },
+  {
+    name: "store",
+    "button text": [
+      "Buy 10 health (10 gold)",
+      "Buy weapon (30 gold)",
+      "Go to town square",
+    ],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store.",
+  },
+  {
+    name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters.",
+  },
+];
+
+//initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function update(location) {
+  monsterStats.computedStyleMap.display = "none";
+  console.log(location);
+  button1.innerText = location["button text"][0];
+  button2.innerText = location["button text"][1];
+  button3.innerText = location["button text"][2];
+
+  button1.onclick = location["button functions"][0];
+  button2.onclick = location["button functions"][1];
+  button3.onclick = location["button functions"][2];
+
+  text.innerText = location.text;
+}
+
+function goTown() {
+  update(locations[0]);
+}
+
+function goStore() {
+  update(locations[1]);
+}
+
+function goCave() {
+  update(locations[2]);
+}
+
+function fightDragon() {}
+
+function buyHealth() {
+  if (gold >= 10) {
+    gold -= 10;
+    health += 10;
+    gold.innerText = gold;
+    healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy the health.";
+  }
+}
+
+function buyWeapon() {
+  if (currentWeapon < weapons.length - 1) {
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      gold.innerText = gold;
+      let newWeapon = weapons[currentWeapon].name;
+      text.innerText += "You now have a " + newWeapon + ".";
+      inventory.push(newWeapon);
+      text.innerText += "In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "you do not have enough gold to buy a weappon.";
+    }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innertext = "Sell weapon for 15 gold";
+    button1.onclick = sellWeapon;
+  }
+}
+
+function sellWeapon() {}
+
+function fightSlime() {}
+
+function fightBeast() {}
+
+function fightDragon() {}
