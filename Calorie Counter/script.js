120338ed1cdf7b961f5ca9dd3fd8c755b78bc138
Rajat Dua <rajatdua1994@gmail.com>
2024-01-05 23:13:51 +0530

    cc:write logic for clearing out the form

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -101,4 +101,18 @@ function getCaloriesFromInputs(list) {
   return calories;
 }
 
+function clearForm() {
+  const inputContainers = Array.from(
+    document.querySelectorAll(".input-container")
+  );
+
+  for (let i = 0; i < inputContainers.length; i++) {
+    inputContainers[i].innerHTML = "";
+  }
+
+  budgetNumberInput.value = "";
+  output.innerText = "";
+  output.classList.add("hide");
+}
+
 addEntryButton.addEventListener("click", addEntry);
